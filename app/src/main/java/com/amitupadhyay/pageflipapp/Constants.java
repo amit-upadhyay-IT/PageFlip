c8b606c3531e2621f4ad1e3740d9397bccb68edc
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-01-02 11:36:24 +0530

    first commit

---
+++ app/src/main/java/com/amitupadhyay/pageflipapp/Constants.java
@@ -0,0 +1,13 @@
+package com.amitupadhyay.pageflipapp;
+
+/**
+ * Created by aupadhyay on 1/1/17.
+ */
+
+public class Constants {
+
+    public final static String PREF_MESH_PIXELS = "MeshPixels";
+    public final static String PREF_DURATION    = "Duration";
+    public final static String PREF_PAGE_MODE   = "PageMode";
+
+}
